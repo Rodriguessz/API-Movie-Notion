5010631f130c0aea483940521ca6dc59bf9c2b62
Enzo <enzo.orodrigues03@gmail.com>
2024-11-04 11:28:02 -0300

    Adicionando middleware cors para permitir requisições externas

--- src/server.js
+++ src/server.js
@@ -2,25 +2,31 @@
 require("express-async-errors")
 
 const express = require("express");
-const app = express();
 const router = require('./routes')
 const dbConnection = require('./database/sqlite');
 const errorCatcher = require("./utils/ErrorCatcher")
 
-//#region Express App Configs
+const cors = require('cors');
 
+
+const app = express();
+
+//#region Express App Configs
 dbConnection()
+
+//Allows requests from different origins
+app.use(cors());
+
 //Configures the application to accept and process JSON data in the body of incoming requests.
 app.use(express.json())
 
 app.use(router)
-
-
+ 
 //Middleware to error treatment on application. It catches the errors that ocurred anywhere on the app
 app.use((error, request, response, next) => {errorCatcher(error,request, response, next)});
 
 
-app.listen(3333, () => console.log(`Server ON: http://locahost:3333`))
+app.listen(3333, () => console.log(`Server ON: http://localhost:3333`))
 
 //#endregion
 
